7d96d072e9f083905a9426150ac8ef7f9313b941
jessicabrunhara <60979214+jessicabrunhara@users.noreply.github.com>
2020-03-25 21:10:03 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-const name = prompt("Olá, jogador! Qual é o seu nome?)"
+const name = prompt("Olá, jogador! Qual é o seu nome?");
 
 const startTest = confirm("Deseja iniciar teste?");
 
-if()
+if (!starTest) {
+    alert("Você não iniciou o teste!");
+    throw new Error("Encerrar o programa.");
+}
+
+const selectedTest = prompt("Qual teste você deseja iniciar? \n 1. História da Moda \n 2. História da Arte");
+
